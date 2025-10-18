d0e74a5d1d3a3ca6579e5a38d0ec465b8e4b8ac4
Emin Samadzade <eminsemedzade7@gmail.com>
2025-10-18 16:25:19 +0400

    Initial project setup with Gradle and basic config

---
+++ src/main/java/com/devees/authaudit/AuthAuditApplication.java
@@ -0,0 +1,13 @@
+package com.devees.authaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthAuditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devees/authaudit/AuthAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devees.authaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
